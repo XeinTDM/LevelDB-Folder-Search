5b05cd0687acb4d462a3ea425bfb012f31105dcd
Xein <100153223+XeinTDM@users.noreply.github.com>
2025-01-28 20:36:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Search;
+
+class Program
+{
+    [DllImport("LevelDBSearch.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void search_all_drives(
+        [MarshalAs(UnmanagedType.LPWStr)] string folder_name,
+        [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr)] string[] excluded_paths,
+        int excluded_paths_count);
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        string folderName = "leveldb";
+        string[] excludedPaths = [@"C:\Windows", @"C:\Program Files", @"C:\Program Files (x86)"];
+
+        Console.WriteLine("Searching a specific directory:");
+        search_all_drives(folderName, excludedPaths, excludedPaths.Length);
+        Console.WriteLine("\nPress any key to exit..."); _ = Console.ReadKey();
+    }
+}
